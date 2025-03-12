df3e6ad1b8071523857162b39c04637f3ee5f1c8
vasya <someemail@somedomen.com>
2025-03-12 22:48:37 +0300

    ccc

--- src/manager/train_simulate.py
+++ src/manager/train_simulate.py
@@ -9,12 +9,13 @@ class TrainState(Enum):
     LOADING = 2
     # Разгружаемся
     GIVEAWAY = 3
-
+    WAITING = 4
 class TrainSimulator:
-    def __init__(self, train: Train, state: TrainState, simulation: Simulation):
+    def __init__(self, train: Train, simulation: Simulation):
         self.train = train
-        self.state: TrainState = state
         self.simulation = simulation
+        # TODO: определить состояние поезда
+        self.state: TrainState = state
 
 
     def step(self):
@@ -25,7 +26,8 @@ class TrainSimulator:
         Здесь мы двигаем поезд вперед
         и если надо обращаемся к другим состояниям
         """
-        # Найти дорогу по которой мы сейчас двигаемся
+        # Найти дорогу по которой мы сейчас двигаемся; 1 - traveled_dist
+        # self.state = TrainState.GIVEAWAY (в  конце чекаем  волюм)
         self.simulation.manager.roads
         self.train.road
         self.train.position["traveled_dist"] += ...
@@ -34,12 +36,12 @@ class TrainSimulator:
     def step_loading(self):
         """
         Обратиться к терминалу в котором мы сейчас находимся за топливом. 
-        Необходимо проверить есть ли доступное топливо
+        Необходимо проверить есть ли доступное топливо; 2 - терминалы сток и загрузка
         """
         pass
 
     def step_giveaway(self):
         """
-        Аналогично предыдущему, но с выгрузкой топлива
+        Аналогично предыдущему, но с выгрузкой топлива; 2  - терминалы сток и выгрузка
         """
         pass
